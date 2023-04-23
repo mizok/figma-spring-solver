580e0273a77c4406cd0262a6bea9f630e0611649
mizok <mizok.huang@neutec.com.tw>
2023-04-23 19:33:52 +0800

    update

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,28 @@
 import { SpringSolver } from './lib/lib';
 
 function main(){
-    const spr=  new SpringSolver(0.5,0.01,0.001,10);
-    
+   
+    const input1 = document.getElementById('input1') as HTMLInputElement;
+    const input2 = document.getElementById('input2') as HTMLInputElement;
+    const input3 = document.getElementById('input3') as HTMLInputElement;
+    const input4 = document.getElementById('input4') as HTMLInputElement;
+
+    const setInstance = ()=>{
+        return spr = new SpringSolver(
+            parseFloat(input1.value),
+            parseFloat(input2.value),
+            parseFloat(input3.value),
+            parseFloat(input4.value),
+        )
+    }
+
+    let spr:SpringSolver = setInstance();
+
+    [input1,input2,input3,input4].forEach((o)=>{
+        o.addEventListener('change',(ev)=>{
+            spr = setInstance()
+        })
+    })
 
     const animate = (ele:HTMLElement)=>{
         const beginTime = performance.now();
@@ -17,10 +37,14 @@ function main(){
         requestAnimationFrame(repeater);
     }
 
-    document.documentElement.addEventListener('click',()=>{
+    document.querySelector('#go').addEventListener('click',()=>{
         const appleEle = document.querySelector('.apple') as HTMLElement;
         animate(appleEle);
     })
+
+    
+
+    
 }
 
 main();
