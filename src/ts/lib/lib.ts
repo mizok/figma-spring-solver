43809927c4842eef48aaddd2b18dea17e16eca84
mizok <mizok.huang@neutec.com.tw>
2023-04-23 19:15:38 +0800

    update

---
+++ src/ts/lib/lib.ts
@@ -0,0 +1,41 @@
+export class SpringSolver {
+    private m_w0!:number;
+    private m_zeta!:number;
+    private m_wd!:number;
+    private m_A!:number;
+    private m_B!:number;
+    constructor( public mass:number,  public stiffness:number,  public damping:number,  public initialVelocity:number){
+        this.init();
+    }
+       
+    private init(){
+        this.m_w0 = Math.sqrt(this.stiffness / this.mass);
+        this.m_zeta = this.damping / (2 * Math.sqrt(this.stiffness * this.mass));
+    
+        if (this.m_zeta < 1) {
+            // Under-damped.
+            this.m_wd = this.m_w0 * Math.sqrt(1 - this.m_zeta * this.m_zeta);
+            this.m_A = 1;
+            this.m_B = (this.m_zeta * this.m_w0 + -this.initialVelocity) / this.m_wd;
+        } else {
+            // Critically damped (ignoring over-damped case for now).
+            this.m_A = 1;
+            this.m_B = -this.initialVelocity + this.m_w0;
+        }
+    }
+    
+    solve(t:number)
+    {
+        if (this.m_zeta < 1) {
+            // Under-damped
+            t = Math.exp(-t * this.m_zeta * this.m_w0) * (this.m_A * Math.cos(this.m_wd * t) + this.m_B * Math.sin(this.m_wd * t));
+        } else {
+            // Critically damped (ignoring over-damped case for now).
+            t = (this.m_A + this.m_B * t) * Math.exp(-t * this.m_w0);
+        }
+    
+        // Map range from [1..0] to [0..1].
+        return 1 - t;
+    }
+    
+}
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,26 @@
-(() => {
+import { SpringSolver } from './lib/lib';
 
-})()
+function main(){
+    const spr=  new SpringSolver(0.5,0.01,0.001,10);
+    
+
+    const animate = (ele:HTMLElement)=>{
+        const beginTime = performance.now();
+        const repeater =  ()=>{
+            const dist  =spr.solve(performance.now() - beginTime)
+            if(Math.abs(dist) > 0){
+                ele.style.transform = `translateY(${dist}px)`
+                requestAnimationFrame(repeater);
+            }
+        }
+
+        requestAnimationFrame(repeater);
+    }
+
+    document.documentElement.addEventListener('click',()=>{
+        const appleEle = document.querySelector('.apple') as HTMLElement;
+        animate(appleEle);
+    })
+}
+
+main();
